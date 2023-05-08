8f6c3c356e156e1bf9307df015a85489c6eacdd8
guimassoqueto <gui.massoqueto@gmail.com>
2023-05-08 18:34:03 -0300

    feat: add main file

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
-def name(a: str) -> int:
-    return 7
+from app.marketplaces.amazon import generate_product_info_dict
+
+generate_product_info_dict(
+    'https://www.amazon.com.br/100-Pure-Creatine-300MG-Integralmedica/dp/B07L5WFHXW')
