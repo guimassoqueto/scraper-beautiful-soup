ddba21baa3ace96e608f701179e6d9a9e4855653
guimassoqueto <gui.massoqueto@gmail.com>
2023-07-07 07:49:54 -0300

    feat: update postgres and main

--- main.py
+++ main.py
@@ -4,19 +4,8 @@ from app.helpers.fake_header import fake_header
 from app.spiders.amazon import get_item
 
 
-def write_query(pid_errors: str = "pid_errors.log", output_filename: str = "query.txt"):
-    query_pids = []
-    with open(pid_errors, "r", encoding="utf-8") as file:
-        for product_id in file:
-            product_id = product_id.strip()
-            query_pids.append(f"('{product_id}')")
-
-    with open(output_filename, "w", encoding="utf-8") as f:
-        f.write(", \n".join(query_pids))
-
-
 def get_failed_products_generator(
-    pid_errors: str = "pid_errors.log",
+    pid_errors: str = "insertion_errors.log",
 ) -> Generator[str, None, None]:
     pids = []
     with open(pid_errors, "r", encoding="utf-8") as file:
@@ -29,22 +18,22 @@ def get_failed_products_generator(
 
 def write_errors(line: str):
     print(line)
-    with open("errors.log", "a", encoding="utf-8") as f:
+    with open("errors.log", "w", encoding="utf-8") as f:
         f.write(f"{line}\n")
 
 
-write_query()
-
-# if __name__ == "__main__":
-#     failed_pids = get_failed_products_generator('errors.log')
-#     count = 1
-#     for pid in failed_pids:
-#       try:
-#         pg = PostgresSingleton()
-#         pg.insert_item('products', get_item(pid, fake_header()))
-#         print(f"inserted #{count}: {pid}", )
-#         count += 1
-#       except Exception as e:
-#          print(e)
-#          write_errors(f'{pid}')
-#          continue
+if __name__ == "__main__":
+    failed_pids = get_failed_products_generator()
+    count = 1
+    for pid in failed_pids:
+        try:
+            pg = PostgresSingleton()
+            pg.insert_item("products", get_item(pid, fake_header()))
+            print(
+                f"inserted #{count}: {pid}",
+            )
+            count += 1
+        except Exception as e:
+            print(e)
+            write_errors(f"{pid}")
+            continue
