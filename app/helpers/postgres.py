4674dd523f6aa6efd564982c4d670e9a2728e562
guimassoqueto <gui.massoqueto@gmail.com>
2023-07-08 15:31:16 -0300

    feat: use aynchronous postgres with psycopg

--- app/helpers/postgres.py
+++ app/helpers/postgres.py
@@ -1,5 +1,4 @@
-import psycopg2
-from threading import Lock
+import psycopg
 
 
 def upsert_query(table: str, item: dict) -> str:
@@ -23,87 +22,17 @@ def upsert_query(table: str, item: dict) -> str:
 
 
 class PostgresDB:
-    def __init__(self, host: str, user: str, password: str, dbname: str):
-        # Create/Connect to database
-        self.connection = psycopg2.connect(
-            host=host,
-            user=user,
-            password=password,
-            dbname=dbname,
-        )
+    def __init__(
+        self,
+        dbname: str = "postgres",
+        dbuser: str = "postgres",
+        dbpassword: str = "password",
+        dbhost: str = "localhost",
+        dbport: str = "5432",
+    ) -> None:
+        self.conninfo = f"dbname={dbname} user={dbuser} password={dbpassword} host={dbhost} port={dbport}"
 
-        # Create cursor, used to execute commands
-        self.cur = self.connection.cursor()
-
-    def insert_item(self, table: str, item: dict) -> None:
-        self.cur.execute(upsert_query(table, item))
-        self.connection.commit()
-
-    def select_non_inserted_ids(self, query: str) -> list:
-        self.cur.execute(query)
-        results = self.cur.fetchall()
-        return results
-
-
-class SingletonMetaThreadSafe(type):
-    _instances = {}
-    _lock: Lock = Lock()
-
-    def __call__(cls, *args, **kwargs):
-        with cls._lock:
-            if cls not in cls._instances:
-                instance = super().__call__(*args, **kwargs)
-                cls._instances[cls] = instance
-        return cls._instances[cls]
-
-
-class PostgresSingletonSafe(metaclass=SingletonMetaThreadSafe):
-    def __init__(self):
-        self.connection = psycopg2.connect(
-            host="localhost",
-            user="postgres",
-            password="password",
-            dbname="postgres",
-        )
-
-        self.cur = self.connection.cursor()
-
-    def insert_item(self, table: str, item: dict) -> None:
-        self.cur.execute(upsert_query(table, item))
-        self.connection.commit()
-
-    def select_non_inserted_ids(self, query: str) -> list:
-        self.cur.execute(query)
-        results = self.cur.fetchall()
-        return results
-
-
-class SingletonMetaThreadUnsafe(type):
-    _instances = {}
-
-    def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            instance = super().__call__(*args, **kwargs)
-            cls._instances[cls] = instance
-        return cls._instances[cls]
-
-
-class PostgresSingleton(metaclass=SingletonMetaThreadUnsafe):
-    def __init__(self):
-        self.connection = psycopg2.connect(
-            host="localhost",
-            user="postgres",
-            password="password",
-            dbname="postgres",
-        )
-
-        self.cur = self.connection.cursor()
-
-    def insert_item(self, table: str, item: dict) -> None:
-        self.cur.execute(upsert_query(table, item))
-        self.connection.commit()
-
-    def select_non_inserted_ids(self, query: str) -> list:
-        self.cur.execute(query)
-        results = self.cur.fetchall()
-        return results
+    async def upsert_item(self, table: str, item: dict) -> None:
+        async with await psycopg.AsyncConnection.connect(self.conninfo) as aconn:
+            async with aconn.cursor() as cur:
+                await cur.execute(upsert_query(table, item))
