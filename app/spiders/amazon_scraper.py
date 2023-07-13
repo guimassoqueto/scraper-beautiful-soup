2f483cea86c8ce131a35608f5e342a9d6a19d043
guimassoqueto <gui.massoqueto@gmail.com>
2023-07-13 08:54:21 -0300

    refactor: rename spiders

---
+++ app/spiders/amazon_scraper.py
@@ -0,0 +1,22 @@
+from app.helpers.postgres import PostgresDB
+from app.spiders.amazon_item import get_item
+from aiohttp import ClientSession
+from asyncio import Semaphore
+from bs4 import BeautifulSoup
+
+
+async def amazon_scraper(pid: str, header: dict, limit: Semaphore) -> BeautifulSoup:
+    url = f"https://amazon.com.br/dp/{pid}"
+    async with limit:
+        async with ClientSession(headers=header) as session:
+            async with session.get(url) as response:
+                assert response.status == 200, "Status Code must be 200"
+                try:
+                    body = await response.text()
+                    soup = BeautifulSoup(body, "html.parser")
+                    item = get_item(pid, soup)
+                    pg = PostgresDB()
+                    await pg.upsert_item("products", item)
+                    print("ok: ", pid)
+                except Exception as e:
+                    print("Error:", pid)
