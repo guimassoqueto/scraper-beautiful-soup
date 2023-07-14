c0284d078743adecb826fc292c193eae2750d79c
guimassoqueto <gui.massoqueto@gmail.com>
2023-07-14 08:57:18 -0300

    chore: remove redis variables

--- app/settings.py
+++ app/settings.py
@@ -9,8 +9,4 @@ POSTGRES_USER = getenv("POSTGRES_USER") or "postgres"
 POSTGRES_PASSWORD = getenv("POSTGRES_PASSWORD") or "password"
 POSTGRES_HOST = getenv("POSTGRES_HOST") or "127.0.0.1"
 
-REDIS_HOST = getenv("POSTGRES_HOST") or "127.0.0.1"
-REDIS_PORT = int(getenv("REDIS_PORT")) or 6379
-REDIS_KEY = getenv("REDIS_KEY") or "amazon_pids"
-
 NON_INSERTED_PIDS_FOLDER = getenv("NON_INSERTED_PIDS_FOLDER") or "./non_inserted_pids"
